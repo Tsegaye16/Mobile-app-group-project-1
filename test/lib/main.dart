ed301a6ea49fdf6964e0c3bf5747c31400318dec
Yeshineh21 <135823408+Yeshineh21@users.noreply.github.com>
2023-06-07 20:20:50 +0300

    Update main.dart

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/User_registration_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: UserRegistration(),
     );
   }
